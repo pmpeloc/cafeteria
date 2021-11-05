73d9d3c9c7b0935df61ed219e78bc12545fdea72
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-11-05 19:50:23 -0300

    Añadir Autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 function css() {
   // Compilar sass
   // Pasos: 1- identificar archivo, 2- compilar, 3- guardar el .css
   src('src/scss/app.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(postcss([autoprefixer()]))
     .pipe(dest('build/css'));
 }
 
