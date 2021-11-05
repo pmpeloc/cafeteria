fa1dc9b64b4193222ec1cf8fac06d0d52fa87492
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-11-05 19:42:40 -0300

    Exportando CSS en diferentes estilos

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ const sass = require('gulp-sass')(require('sass'));
 function css() {
   // Compilar sass
   // Pasos: 1- identificar archivo, 2- compilar, 3- guardar el .css
-  src('src/scss/app.scss').pipe(sass()).pipe(dest('build/css'));
+  src('src/scss/app.scss')
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(dest('build/css'));
 }
 
 function dev() {
