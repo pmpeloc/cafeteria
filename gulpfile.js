e3c94eede29c4d992ffc8b7839e445b20b93f6f5
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-11-05 20:17:13 -0300

    Dividir tu código SASS en diferentes archivos

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,31 @@
-const { src, dest, watch } = require('gulp');
+// npm install gulpjs/gulp#v4.0.0-alpha.3 (para usar series)
+const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 
-function css() {
+function css(done) {
   // Compilar sass
   // Pasos: 1- identificar archivo, 2- compilar, 3- guardar el .css
   src('src/scss/app.scss')
-    .pipe(sass({ outputStyle: 'compressed' }))
+    // .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(sass())
     .pipe(postcss([autoprefixer()]))
     .pipe(dest('build/css'));
+  done();
 }
 
 function dev() {
   watch('src/scss/app.scss', css);
 }
 
+function tareaDefault() {
+  console.log('soy la tarea por default');
+}
+
 exports.css = css;
 exports.dev = dev;
+exports.default = series(css, dev);
+
+// series - Se inicia una tarea, y hasta que finaliza, inicia la siguiente
+// parallel - Todas inician al mismo tiempo
